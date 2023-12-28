7cd5457241c846ede871a5ec85303cfcc47ff4a5
sm-git123 <143545156+sm-git123@users.noreply.github.com>
2023-12-28 17:17:45 -0500

    Update RuntimeExample.java

--- src/test/javaconcepts/RuntimeExample.java
+++ src/test/javaconcepts/RuntimeExample.java
@@ -4,6 +4,7 @@ public class RuntimeExample {
 
 	public static void main(String args[]) {
 
+		System.out.println("Inside RuntimeExample main method - from feature2 testing merge conflicts");
 		Runtime rt = getRuntime();
 		System.out.println("runtime::" + rt.toString());
 	}
