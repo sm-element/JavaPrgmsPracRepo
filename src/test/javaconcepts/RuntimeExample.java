fb941d917fc153f59aa53e3edf7d1f1816ccb418
sm-git123 <143545156+sm-git123@users.noreply.github.com>
2023-12-28 17:21:57 -0500

    Testing merge conflicts on the repository for multiple users

--- src/test/javaconcepts/RuntimeExample.java
+++ src/test/javaconcepts/RuntimeExample.java
@@ -5,6 +5,7 @@ public class RuntimeExample {
 	public static void main(String args[]) {
 
 		System.out.println("Inside RuntimeExample main method - from feature2 testing merge conflicts");
+		System.out.println("Inside RuntimeExample main method - from feature2 - committing and pushing changes to feature branch");
 		Runtime rt = getRuntime();
 		System.out.println("runtime::" + rt.toString());
 	}
