cdfa565abe0971a1a8e4bacb13006657b1dfc5d4
sm-git123 <143545156+sm-git123@users.noreply.github.com>
2023-12-29 11:28:55 -0500

    Testing merge conflicts on the repository for multiple users

---
+++ src/test/Git_Tips_Tricks_Testing/Git_Test_Program_2.java
@@ -0,0 +1,9 @@
+package test.javaconcepts;
+
+class Git_Test_Program_2
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Inside Git_Test_Program_2 -> main method");
+	}
+}
---
+++ src/test/Git_Tips_Tricks_Testing/Git_Test_Program_3.java
@@ -0,0 +1,9 @@
+package test.javaconcepts;
+
+class Git_Test_Program_3
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Inside Git_Test_Program_3 -> main method");
+	}
+}
