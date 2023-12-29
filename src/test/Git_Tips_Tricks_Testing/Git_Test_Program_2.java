341b5a477a0ad3c2fe2ce99682821e24c01becba
sm-git123 <143545156+sm-git123@users.noreply.github.com>
2023-12-29 15:41:02 -0500

    Update Git_Test_Program_2.java

--- src/test/Git_Tips_Tricks_Testing/Git_Test_Program_2.java
+++ src/test/Git_Tips_Tricks_Testing/Git_Test_Program_2.java
@@ -5,5 +5,6 @@ class Git_Test_Program_2
 	public static void main(String arg[])
 	{
 		System.out.println("Inside Git_Test_Program_2 -> main method");
+		System.out.println("Inside Git_Test_Program_2 - edited this file in develop branch directly - changes in feature branch made but not committed yet");
 	}
 }
