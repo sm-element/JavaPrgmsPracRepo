ce6bdeb058cc3d984b84177371501deadc41b92b
sm-git123 <143545156+sm-git123@users.noreply.github.com>
2023-12-29 15:44:46 -0500

    From feature 3 testing merge conflicts commit for the change in feature branch.

--- src/test/Git_Tips_Tricks_Testing/Git_Test_Program_2.java
+++ src/test/Git_Tips_Tricks_Testing/Git_Test_Program_2.java
@@ -5,6 +5,6 @@ class Git_Test_Program_2
 	public static void main(String arg[])
 	{
 		System.out.println("Inside Git_Test_Program_2 -> main method");
-		Sytem.out.println("feature 3 - change 1 - after creation of files");
+		Sytem.out.println("feature 3 - change 1 from feature branch- after creation of files");
 	}
 }
