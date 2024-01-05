939b895023684d7ec18ee1a7449d1e3ea8ad655b
sm-git123 <143545156+sm-git123@users.noreply.github.com>
2024-01-05 13:42:28 -0500

    Update Git_Test_Program_3.java

--- src/test/Git_Tips_Tricks_Testing/Git_Test_Program_3.java
+++ src/test/Git_Tips_Tricks_Testing/Git_Test_Program_3.java
@@ -5,5 +5,6 @@ class Git_Test_Program_3
 	public static void main(String arg[])
 	{
 		System.out.println("Inside Git_Test_Program_3 -> main method");
+		System.out.println("Inside Git_Test_Program_3 -> From develop branch - trying merge conflict - direct edit in develop branch in git");
 	}
 }
