22ab94b0c182f2d73307f3689d5fcfb7cbf090da
sm-git123 <143545156+sm-git123@users.noreply.github.com>
2024-01-05 13:43:31 -0500

    From feature branch 4 - 1st commit trying to create merge conflict

--- src/test/Git_Tips_Tricks_Testing/Git_Test_Program_3.java
+++ src/test/Git_Tips_Tricks_Testing/Git_Test_Program_3.java
@@ -4,6 +4,8 @@ class Git_Test_Program_3
 {
 	public static void main(String arg[])
 	{
+
 		System.out.println("Inside Git_Test_Program_3 -> main method");
+		System.out.println("Inside Git_Test_Program_3 -> From feature4 branch - trying to create merge conflict - 1st edit after checkout")
 	}
 }
