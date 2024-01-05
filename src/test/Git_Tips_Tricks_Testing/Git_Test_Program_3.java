eb94be4fcb45e853ddd57a8e25167b96129b94c6
sm-git123 <143545156+sm-git123@users.noreply.github.com>
2024-01-05 14:32:56 -0500

    From feature branch 6 - 1st commit trying to create merge conflict

--- src/test/Git_Tips_Tricks_Testing/Git_Test_Program_3.java
+++ src/test/Git_Tips_Tricks_Testing/Git_Test_Program_3.java
@@ -11,6 +11,6 @@ class Git_Test_Program_3
 
 		System.out.println("From develop branch - for feature5 changes");
 
-		System.out.println("From feature5 branch - first commit from feature branch");
+		System.out.println("From feature6 branch - first commit from feature branch");
 	}
 }
